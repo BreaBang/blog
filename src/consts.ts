273d8471792491eeb17843aab9a33844e362c9c1
breabang <breanna.bang@gmail.com>
2026-09-19 16:05:52 -0600

    personalize

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'TBD';
+export const SITE_DESCRIPTION = 'Find local Colorado and Virtual events to meet other people and forge genuine human connections';
